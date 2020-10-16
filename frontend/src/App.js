ab0fde3c47d1e46570bd20971d1a7b9482438517
DennisMaas <mail@dennismaas.de>
2020-10-16 12:26:37 +0200

    add h1 add button add Header.js add todoview.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import Header from "./components/Header";
+import ToDoView from "./components/ToDoView";
+import React, {useState} from 'react';
 
 function App() {
+    const [toDoList, setToDoList]= useState([]);
+    function addNewToDo(toDo){
+        setToDoList([...toDoList,toDo])
+    }
+
   return (
-    <div >
-      Hallo React start
+    <div>
+      <Header onAddToDo = {addNewToDo}></Header>
+      <ToDoView/>
     </div>
   );
 }
