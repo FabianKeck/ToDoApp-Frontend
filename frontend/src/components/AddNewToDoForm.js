c7fa4763c67437cb977ae8ce2869486f4368de92
Fabian Keck <fabian.keck@posteo.de>
2020-10-16 23:43:26 +0200

    Add Logic to choose Status when Adding a new ToDo

--- frontend/src/components/AddNewToDoForm.js
+++ frontend/src/components/AddNewToDoForm.js
@@ -1,13 +1,16 @@
 import React, {useState} from "react";
 import styled from "styled-components/macro";
 import axios from "axios";
+import getStatusList from "../utils/getStatusList";
+import getNeatStatusString from "../utils/getNeatStatusString";
 
 export default function AddNewToDoForm({onSubmit}){
     const [description, setDescription] = useState("Enter Description");
+    const [status, setStatus] = useState("OPEN");
 
     function postNewToDo(){
         console.log("Test Todo??")
-        axios.post("/api/todo", {description:description, status:"OPEN"})
+        axios.post("/api/todo", {description:description, status:status})
             .then(response => response.data)
             .then(onSubmit)
 
@@ -17,6 +20,11 @@ export default function AddNewToDoForm({onSubmit}){
 
             <StyledToDoForm>
                 <label>Please enter description of your new To-Do:</label><br/>
+                <select onChange={event=>setStatus(event.target.value)}>
+                    {getStatusList().map(status=>
+                        <option value={status}>{getNeatStatusString(status)}</option>)
+                    }
+                </select>
                 <input onChange={event => setDescription(event.target.value)} type={"text"} value={description}/>
                 <input type={"button"} value={"addToDo"} onClick={postNewToDo}/>
             </StyledToDoForm>
